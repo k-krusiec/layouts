a93c01ef22b5e8f0e7697139fe247c4b9df2aed5
Karol Krusiec <karol@krusiec.com>
2017-10-07 22:00:28 +0200

    added mobile menu support

--- layout004/app/js/main.js
+++ layout004/app/js/main.js
@@ -1,1 +1,14 @@
-// log
+document.addEventListener('DOMContentLoaded', () => {
+  (function () {
+    const mainNav = document.querySelector('.main-navigation');
+    const list = mainNav.querySelector('ul');
+    const hamburgerBtn = mainNav.querySelector('.hamburger-btn');
+
+    hamburgerBtn.addEventListener('click', () => {
+      hamburgerBtn.firstElementChild.classList.toggle('hidden');
+      hamburgerBtn.lastElementChild.classList.toggle('hidden');
+      list.classList.toggle('hidden');
+      mainNav.classList.toggle('fade-in');
+    })
+  })();
+});
